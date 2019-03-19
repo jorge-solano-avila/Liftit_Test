86a8baa357466f8fed68ef33ffd428bb88d25aea
Jorge Solano <jorgesolavila@gmail.com>
2019-03-19 16:21:41 -0500

    Feat: add global reducers
    Add initial global reducers

---
+++ src/reducers.ts
@@ -0,0 +1,15 @@
+interface State {
+
+}
+
+interface Action {
+  type: string
+}
+
+const initialState = {
+  
+};
+
+export const reducers = (state: State = initialState, action: Action) => {
+  return state
+}
