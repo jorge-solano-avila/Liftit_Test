ac409e39d207cbeb3bcd1589e8391ba4d8769a46
Jorge Solano <jorgesolavila@gmail.com>
2019-03-19 16:27:40 -0500

    Chore: update file hierarchy
    Update containers folder

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import './styles.scss';
 
-export class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="app">Test</div>
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import store from './store';
-import { App } from './components/App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
