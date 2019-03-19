ee039818799baf8b6a0ca02d46fd3cb20b6a5d8f
Jorge Solano <jorgesolavila@gmail.com>
2019-03-19 18:09:07 -0500

    Feat: add log in container
    Add initial files

---
+++ src/containers/LogIn/index.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './styles.scss';
+
+export default class LogIn extends Component {
+  render() {
+    return (
+      <div className="log-in">Test2</div>
+    );
+  }
+}
