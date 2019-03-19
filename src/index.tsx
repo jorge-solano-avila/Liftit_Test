91054fed33c153ba09d9d2404ca5a850c92510c8
Jorge Solano <jorgesolavila@gmail.com>
2019-03-19 17:56:33 -0500

    Feat: update index
    Import Root container

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 
 import store from './store';
-import App from './containers/App';
+import Root from './containers/Root';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Root store={store} />,
   document.getElementById('root')
 );
 
