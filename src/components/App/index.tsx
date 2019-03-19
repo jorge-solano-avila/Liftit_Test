16b0a1c251e08c074b60c01c507a152945c53e73
Jorge Solano <jorgesolavila@gmail.com>
2019-03-19 12:25:58 -0500

    Chore: update files hierarchy
    Update components hierarchy

---
+++ src/components/App/index.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './styles.scss';
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="app">Test</div>
+    );
+  }
+}
