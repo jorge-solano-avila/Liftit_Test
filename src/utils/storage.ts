24f2a04aaec9b2d0f4c6fddca0535ee90a002a17
Jorge Solano <jorgesolavila@gmail.com>
2019-03-20 15:54:37 -0500

    Feat: add storage util
    Add initial functions

---
+++ src/utils/storage.ts
@@ -0,0 +1,11 @@
+export const getLocalItem = (key: string): any => {
+  const item: string | null = localStorage.getItem(key);
+
+  return item ? JSON.parse(item) : item;
+}
+
+export const saveLocalItem = (key: string, data: any): void => {
+  const item: string = JSON.stringify(data);
+
+  localStorage.setItem(key, item);
+}
