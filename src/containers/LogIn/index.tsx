ee41917147bbe6f3e7ec9d0969216cdd27480592
Jorge Solano <jorgesolavila@gmail.com>
2019-03-20 09:51:17 -0500

    Feat: update log in container
    Add redux connect

--- src/containers/LogIn/index.tsx
+++ src/containers/LogIn/index.tsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import './styles.scss';
 
-export default class LogIn extends Component {
+class LogIn extends Component {
   render() {
     return (
       <div className="log-in">Test2</div>
     );
   }
 }
+
+const mapStateToProps = (state: any, ownProps: any) => ({
+
+});
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    dispatch,
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LogIn);
