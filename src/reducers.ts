63961e0a4bb744dfffb9fc8df211c6c7d4540e98
Jorge Solano <jorgesolavila@gmail.com>
2019-03-20 15:29:41 -0500

    Feat: update global reducers
    Add log in reducer

--- src/reducers.ts
+++ src/reducers.ts
@@ -1,14 +1,18 @@
 import { combineReducers, Reducer } from "redux";
 import { reducer as formReducer, FormStateMap } from "redux-form";
 
+import logInReducer, { LogInState } from "./containers/LogIn/reducer";
+
 export interface Action {
   type: string;
 }
 
 export interface Reducers {
+  logIn: LogInState,
   form: FormStateMap;
 }
 
 export const rootReducer: Reducer<Reducers, Action> = combineReducers({
+  logIn: logInReducer,
   form: formReducer
 });
