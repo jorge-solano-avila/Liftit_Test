4aa05db7fb0cd6e60fe0b51834027d04b228e100
Jorge Solano <jorgesolavila@gmail.com>
2019-03-20 16:05:12 -0500

    Feat: update app container
    Add logic of authentication

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,24 +1,38 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import './styles.scss';
+import "./styles.scss";
+import { Session } from "../LogIn";
+import { getLocalItem } from "../../utils/storage";
 
-class App extends Component {
+interface AppProps {
+  history: any;
+}
+
+class App extends Component<AppProps, {}> {
+  componentDidMount() {
+    const { history } = this.props;
+    const session: Session = getLocalItem("session");
+
+    if (!session) {
+      history.replace("/log-in");
+    }
+  }
+
   render() {
-    return (
-      <div className="app">Test</div>
-    );
+    return <div className="app">Test</div>;
   }
 }
 
-const mapStateToProps = (state: any, ownProps: any) => ({
-
-});
+const mapStateToProps = (state: any, ownProps: any) => ({});
 
 const mapDispatchToProps = (dispatch: any) => {
   return {
-    dispatch,
+    dispatch
   };
-}
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
