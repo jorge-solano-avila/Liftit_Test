7eacbc70167142e999d01b68965fc42fce9f2857
Jorge Solano <jorgesolavila@gmail.com>
2019-03-20 09:50:49 -0500

    Feat: update app container
    Add redux connect

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import './styles.scss';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
       <div className="app">Test</div>
     );
   }
 }
+
+const mapStateToProps = (state: any, ownProps: any) => ({
+
+});
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    dispatch,
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
