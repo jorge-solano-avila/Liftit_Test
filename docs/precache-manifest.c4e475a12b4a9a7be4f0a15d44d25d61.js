17f87843ab7e0482766f101040752a0062b0ca3a
Jorge Solano <jorgesolavila@gmail.com>
2019-03-21 01:12:54 -0500

    Chore: add docs folder
    Add static files

---
+++ docs/precache-manifest.c4e475a12b4a9a7be4f0a15d44d25d61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "720661879a87997bbce8",
+    "url": "/static/js/main.d7b3ba34.chunk.js"
+  },
+  {
+    "revision": "1406015851ef96996d37",
+    "url": "/static/js/2.11d1cdbb.chunk.js"
+  },
+  {
+    "revision": "720661879a87997bbce8",
+    "url": "/static/css/main.e0c34c51.chunk.css"
+  },
+  {
+    "revision": "91d625c881b067399d85afceaed8b6bc",
+    "url": "/index.html"
+  }
+];
