066898afc8064f95669f945be73c06b6ee3a1eef
Jorge Solano <jorgesolavila@gmail.com>
2019-03-21 01:33:47 -0500

    Updates

---
+++ precache-manifest.1cd34675bc93bcb86d9d0333dcb53671.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7f22b5ea6b95a191cb2",
+    "url": "/Liftit_Test/static/js/runtime~main.790ae98d.js"
+  },
+  {
+    "revision": "fe24762c1d7d677d42b0",
+    "url": "/Liftit_Test/static/js/main.5c533696.chunk.js"
+  },
+  {
+    "revision": "1406015851ef96996d37",
+    "url": "/Liftit_Test/static/js/2.11d1cdbb.chunk.js"
+  },
+  {
+    "revision": "fe24762c1d7d677d42b0",
+    "url": "/Liftit_Test/static/css/main.e0c34c51.chunk.css"
+  },
+  {
+    "revision": "4bfd45db2164501c89e7d99f11b5962c",
+    "url": "/Liftit_Test/index.html"
+  }
+];
