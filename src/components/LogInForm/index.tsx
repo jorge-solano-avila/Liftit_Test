9b885673be1b42ad2d30da188ce0f20138b04fe1
Jorge Solano <jorgesolavila@gmail.com>
2019-03-20 23:37:28 -0500

    Fix: update log in form component
    Add max length validations and general refactoring

--- src/components/LogInForm/index.tsx
+++ src/components/LogInForm/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component, InputHTMLAttributes } from "react";
+import React, { InputHTMLAttributes } from "react";
 import { Field, reduxForm, FormErrors } from "redux-form";
 
 import "./styles.scss";
@@ -22,12 +22,16 @@ const validate = (values: any): FormErrors<any, string> => {
     errors.email = "Debes ingresar un correo";
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
     errors.email = "Debes ingresar un correo válido";
+  } else if (email.length > 255) {
+    errors.email = "El correo debe tener máximo 255 caracteres";
   }
 
   if (!password) {
     errors.password = "Debes ingresar una contraseña";
   } else if (password.length < 8) {
     errors.password = "La contraseña debe tener mínimo 8 caracteres";
+  } else if (password.length > 255) {
+    errors.password = "La contraseña debe tener máximo 255 caracteres";
   }
 
   return errors;
@@ -72,7 +76,7 @@ let LogInForm = (props: any) => {
         label="Contraseña"
       />
       <div>
-        <button type="submit">Log in</button>
+        <button type="submit">Ingresar</button>
       </div>
     </form>
   );
